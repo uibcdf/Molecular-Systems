74c4313ae25584ad24bea65f961280f187eda9cb
Diego Prada <prada.gracia@gmail.com>
2022-02-25 14:49:54 -0600

    Tests fixed

--- uibcdf_systems/tests/test_import.py
+++ uibcdf_systems/tests/test_import.py
@@ -3,11 +3,11 @@ Unit and regression test for the openmolecularsystems package.
 """
 
 # Import package, test suite, and other packages as needed
-import openmolecularsystems
+import uibcdf_systems
 import pytest
 import sys
 
-def test_openmolecularsystems_imported():
+def test_uibcdf_systems_imported():
     """Sample test, will always pass so long as import statement worked"""
-    assert "openmolecularsystems" in sys.modules
+    assert "uibcdf_systems" in sys.modules
 
